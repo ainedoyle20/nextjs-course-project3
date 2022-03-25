e85b94bb5deb5187ef4c2485d86b2e5a2d2f4ca4
Aine Doyle <ainedoyleAD@gmail.com>
2022-03-25 21:18:21 +0000

    final commmit for project 3 in nextjs course, module 9 and 10, API Routes and WReact context

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,8 +1,56 @@
+import { useRef, useContext } from 'react';
+
+import NotificationContext from '../../store/notification-context';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const emailInput = useRef();
+  const notificationCtx =  useContext(NotificationContext);
+  
   function registrationHandler(event) {
     event.preventDefault();
+    
+
+    const enteredEmail = emailInput.current.value;
+    const reqBody = { email: enteredEmail };
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter',
+      status: 'pending',
+    });
+
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then(data => {
+          throw new Error(data.message || 'Something went wrong!')
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success',
+          message: 'Successfully registered for newsletter',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        console.log('error: ', error);
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
 
     // fetch user input (state or refs)
     // optional: validate input
@@ -19,8 +67,9 @@ function NewsletterRegistration() {
             id='email'
             placeholder='Your email'
             aria-label='Your email'
+            ref={emailInput}
           />
-          <button>Register</button>
+          <button onClick={registrationHandler}>Register</button>
         </div>
       </form>
     </section>
---
+++ pages/api/newsletter.js
@@ -0,0 +1,34 @@
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
+        const userEmail = req.body.email;
+
+        if (!userEmail || !userEmail.includes('@')) {
+            res.status(422).json({ message: 'Invalid email address' });
+        }
+
+        let client;
+
+        try {
+            client = await connectDatabase();
+        } catch (error) {
+            res.status(500).json({ message: 'Connecting to the database failed!' });
+            return;
+        }
+
+        try {
+            await insertDocument(client, 'newsletter', { email: userEmail });
+            client.close();
+        } catch (error) {
+            res.status(500).json({ message: 'Inserting data failed!' });
+            return;
+        }
+
+        res.status(201).json({ message: 'Signed up' });
+    } else {
+        res.status(200).json({ message: 'This is working '});
+    }
+}
+
+export default handler;
